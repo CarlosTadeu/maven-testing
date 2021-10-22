2282de6129da3699d0b3ed318d7d354058102a6b
Carlos Tadeu <carlos.santos@opus-software.com.br>
2021-10-22 11:38:11 -0300

    add pojo unit test

---
+++ src/test/java/JavaHelloWorldTest.java
@@ -0,0 +1,7 @@
+public class JavaHelloWorldTest {
+
+    public void testGetHello() {
+        JavaHelloWorld javaHelloWorld = new JavaHelloWorld();
+        assert("Hello World".equals(javaHelloWorld.getHello()));
+    }
+}
